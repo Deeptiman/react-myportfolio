f99f687115485a5105e5f974216ce86bc08dfd94
Deeptiman Pattnaik <pattnaikdeeptiman@gmail.com>
2020-08-03 02:25:26 +0530

    Portfolio Design

--- src/components/ProjectsComponent.js
+++ src/components/ProjectsComponent.js
@@ -1,8 +1,8 @@
 import React, { Component } from 'react';
 import '../index.css';
 
-import banner from '../images/hyperledger_multi_org_banner.png';
-
+import medium from '../images/medium_logo.png';
+import github from '../images/github_logo.png';
 import {fetchPortfolioLists} from '../actions';
 import { connect } from 'react-redux';
 
@@ -13,25 +13,84 @@ class ProjectsComponent extends Component {
         this.props.fetchPortfolioLists()
     }
 
-    renderPortfolioDetails = (portfolioItem,index) => {
-        console.log("Name -- "+portfolioItem.name)
-        return (
-            <div className={'project-items-container'}>
+    checkServer = (portfolioItem) => {
 
+        if(portfolioItem.hasGithub === true) {
+            return (
+                <div className={'project-link-container'}>
+
+                    <div className={'personal-project-container'}>    
+
+                        {portfolioItem.hasMedium === true &&
+                            <div className={'medium-blog-container'}>
+                                <a href={portfolioItem.medium} target="_blank"><img src={medium} className={'medium'}/></a>
+                            </div>                        
+                        }
+
+                        <div className={'github-repo-container'}>
+                            <a href={portfolioItem.github} target="_blank">
+                                <img src={github} className={'github-logo-buttton'} />    
+                            </a>
+                            <div className={'github-badge-container'}>
+                                <img alt="GitHub last commit" src={portfolioItem.github_badges_last_commit} className={'github-badge-icon'}/> 
+                                <img alt="GitHub language count" src={portfolioItem.github_badges_language_count} className={'github-badge-icon'}/>
+                                <img alt="GitHub top language" src={portfolioItem.github_badges_top_language} className={'github-badge-icon'}/>
+                                <img alt="GitHub forks" src={portfolioItem.github_badges_forks} className={'github-badge-icon'}/>
+                            </div> 
+                        </div>
+                    </div>
+                </div>
+            )
+        }
+
+        if(portfolioItem.hasPlaystore === true) {
+            return (
+                <div className={'project-link-container'}>
+                    <a href={portfolioItem.playstore} target="_blank">
+                        <img alt='Get it on Google Play' src='https://play.google.com/intl/en_us/badges/static/images/badges/en_badge_web_generic.png' className={'playstore-badge-icon'}/>
+                    </a>
+                </div>
+            )
+        }
+    }
+
+    renderEvenPortfolioDetails = (portfolioItem,index) => {
+        console.log("Name -- "+portfolioItem.name)
+       
+        return (
+            <div className={'project-even-items-container'}>                
                     <div className={'project-thumbnail-item'}>
                         <img src={portfolioItem.banner} className={'banner'} />
                     </div>
                     <div className={'project-details-container'}>
-
-                        <div className={'project-name-container'}>
-                            {portfolioItem.name}
-                        </div>
-
+                        
                         <div className={'project-description-container'}>
-                            {portfolioItem.description}
+                           <p className={'project-description'}> {portfolioItem.description} </p>
                         </div>
+                        {this.checkServer(portfolioItem)}
+                    </div>                
+            </div>
+        )
+    }
 
-                    </div>
+    renderOddPortfolioDetails = (portfolioItem,index) => {
+        console.log("Name -- "+portfolioItem.name)
+        
+        return (
+            <div className={'project-odd-items-container'}>                
+                    <div className={'project-details-container'}>
+                         
+                        <div className={'project-description-container'}>
+                            <p className={'project-description'}> {portfolioItem.description} </p>
+                        </div>
+                        
+                        {this.checkServer(portfolioItem)}
+
+                    </div>                
+                    <div className={'project-thumbnail-item'}>
+                        <img src={portfolioItem.banner} className={'banner'} />
+                    </div>                   
+                   
             </div>
         )
     }
@@ -40,10 +99,17 @@ class ProjectsComponent extends Component {
         if(portfolioLists.length > 0){
             return (
                 portfolioLists.map((portfolioItem,index) => {
+                    var i = index + 1;
 
-                    return (
-                        <div>{this.renderPortfolioDetails(portfolioItem, index)}</div>
-                    )
+                    if(i%2 == 0){
+                        return (                        
+                            <div>{this.renderEvenPortfolioDetails(portfolioItem, index)}</div>
+                        )
+                    } else if(i%2 !=0){
+                        return (                        
+                            <div>{this.renderOddPortfolioDetails(portfolioItem, index)}</div>
+                        )
+                    }
                 })
             )
         }
---
+++ src/images/medium-logos-master/medium-logos-master/README.md
@@ -0,0 +1,12 @@
+# Medium Logos
+
+This repo contains versions of our logo and wordmark in popular formats. Feel free to
+use these anywhere on the web with a few simple guidelines: Do not modify or alter the
+Medium logo or use it in a confusing way, including suggesting sponsorship or endorsement
+by Medium, or in a way that confuses Medium with another brand.
+
+[For full brand guidelines read this.](https://medium.com/policy/logos-and-brand-guidelines-f1a01a733592)
+
+If you have any questions, email us at yourfriends@medium.com.
+
+Thanks
